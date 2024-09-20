28295ed1bd5456621363298b7e79beabe977c760
Jean Bertrand KAMTCHOUM YONGA <jean.kamtchoumyonga.etu@univ-lille.fr>
2024-09-20 23:48:52 +0200

    BikeStation

--- src/BikeStation/BikeStation.java
+++ src/BikeStation/BikeStation.java
@@ -57,4 +57,25 @@ public class BikeStation{
         // Générer un entier aléatoire entre min et max (inclus)
         return random.nextInt(max - min + 1) + min;
     }
+
+    /**
+     * Two BikeStations are equals if same Id 
+     * @return true if equals, false if not
+     */
+    public boolean equals(Object o) {
+        try {
+            BikeStation Other = (BikeStation) o;
+            return this.getBikeStationId().equals(Other.getBikeStationId());
+        } catch (ClassCastException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Get a description of the BikeStation
+     * @return a description of the BikeStation
+     */
+    public String toString() {
+        return "BikeStation : " + this.getBikeStationId();
+    }
 }
