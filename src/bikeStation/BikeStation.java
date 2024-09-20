ea72574634a510f1af9e802b4483778a0dec80ac
Ramjattan Adam <adam.ramjattan.etu@a10p20.fil.univ-lille.fr>
2024-09-20 13:31:15 +0200

    fin de l'initialisation de la classe bikeStation

--- src/bikeStation/BikeStation.java
+++ src/bikeStation/BikeStation.java
@@ -5,6 +5,7 @@ import Bike.Bike;
 public class BikeStation{
     
     private Bike bikes [];
+    private int nbOfBikes;
     private String bikeStationId;
     private int cmpTimer; /*compte le nombre de fois qu'une sation est vide ou plein apres chaus e fin de timer*/
     private static Random random = new Random();
@@ -14,12 +15,15 @@ public class BikeStation{
         this.bikeStationId =id;
         this.capacity = generateCapacity();
         this.cmpTimer = 0;
+        this.nbOfBikes = 0;
 
+        /*initialise toute les valeurs du tableau a null */
         for (int i=0; i<this.capacity; i++){
             this.bikes[i] = null;
         }
     }
 
+    /*Genere la capacité de la station aléatoirement entre 10 et 20*/
     public int generateCapacity(){
         return 10 + BikeStation.random.nextInt(10);
     }
@@ -41,6 +45,11 @@ public class BikeStation{
         return this.capacity;
     }
 
+    /*renvoie le nombre de bike present dans la station */
+    public int getNbOfBike(){
+        return this.nbOfBikes;
+    }
+
     /*Renvoie le numero du premiere emplacement vide */
     public int firstFreeSlot(){
         for (int i = 0; i<this.capacity; i++){
@@ -52,5 +61,10 @@ public class BikeStation{
                 System.out.println("Il n'y a pas d'emplacement vide");
             }
         }
+    }
+
+    /*Permet de deposer un velo dans le premier emplacement vide de la station */
+    public void dropBike(Bike bike) {
+        
     }
 }
