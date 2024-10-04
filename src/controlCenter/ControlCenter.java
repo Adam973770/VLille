b5bc42f54092f73a622f182acabc3e1502c5bc24
Ramjattan Adam <adam.ramjattan.etu@a14p7.fil.univ-lille.fr>
2024-10-04 08:38:03 +0200

    initialisation de la classe controlCenter sous forme de singleton

--- src/controlCenter/ControlCenter.java
+++ src/controlCenter/ControlCenter.java
@@ -3,5 +3,15 @@ package controlCenter;
  * class for ControlCenter
  */
 public class ControlCenter{
-    /*modif dossier */
+    
+    private static ControlCenter uniqueInstance;
+
+    private ControlCenter(){}
+
+    public static ControlCenter getInstance(){
+        if (uniqueInstance == null) {
+            uniqueInstance = new ControlCenter();
+        }
+        return uniqueInstance;
+    }
 }
