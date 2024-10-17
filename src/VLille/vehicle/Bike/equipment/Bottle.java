85ac3f3093d1f048a87ee798995f97787e9d78ab
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-10-17 13:11:52 +0200

    classe Bottle

---
+++ src/VLille/vehicle/Bike/equipment/Bottle.java
@@ -0,0 +1,57 @@
+/**
+ * The `Bottle` class is a decorator for the `Bike` class, which adds the functionality of a bottle to a bike.
+ * This class is part of the decorator pattern used to dynamically add functionality to bikes in the VLille system.
+ * 
+ * The `Bottle` class extends `DecorateurBike`, which allows it to "decorate" or enhance a `Bike` object
+ * by adding new behavior while keeping the core functionality of the bike intact.
+ */
+package VLille.vehicle.Bike.equipment;
+
+import VLille.vehicle.*;
+import VLille.vehicle.Bike.*;
+
+public class Bottle extends DecorateurBike {
+
+    /**
+     * Constructs a new `Bottle` decorator for the specified `Bike` object.
+     * 
+     * @param b The `Bike` object that is being decorated with a bottle.
+     */
+    public Bottle(Bike b) {
+        bike = b;
+    }
+
+    /**
+     * Returns the label (libelle) of the bike with the added information about the bottle.
+     * 
+     * @return A string representing the bike's label, which now includes "+ Bottle".
+     */
+    @Override
+    public String getLibelle() {
+        return bike.getLibelle() + "+ Bottle ";
+    }
+
+    /**
+     * Returns the number of usages (nbUsage) for the bike.
+     * 
+     * This method simply forwards the request to the decorated bike object.
+     * 
+     * @return The number of usages of the bike.
+     */
+    @Override
+    public int getNbUsage() {
+        return bike.getNbUsage();
+    }
+
+    /**
+     * Returns the bike's unique ID (bikeId).
+     * 
+     * This method simply forwards the request to the decorated bike object.
+     * 
+     * @return The bike's unique ID.
+     */
+    @Override
+    public String getBikeId() {
+        return bike.getBikeId();
+    }
+}
