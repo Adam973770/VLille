307bf0d7d7a141f93ce59a09186cfce5aabbb7dc
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-10-17 12:57:05 +0200

    classe Renter

---
+++ src/VLille/user/Renter.java
@@ -0,0 +1,65 @@
+package VLille.user;
+import VLille.vehicle.Bike.*;
+
+public class Renter extends User{
+
+    public Renter (RenterBuilder builder){
+        this.firstName = builder.firstName;
+        this.secondName = builder.secondName;
+        this.age = builder.age;
+        this.gender = builder.gender;
+        this.adress = builder.adress;
+        this.numTel = builder.numTel;
+        this.ownedBike = builder.ownedBike;
+    }
+
+    public static class RenterBuilder {
+        private String firstName;
+        private String secondName;
+        private int age;
+        private String gender;
+        private String adress;
+        private String numTel;
+        private Bike ownedBike; 
+
+        public RenterBuilder setFirstName(String name){
+            this.firstName = name;
+            return this;
+        }
+
+        public RenterBuilder setSecondName(String name){
+            this.secondName = name;
+            return this;
+        }
+
+        public RenterBuilder setAge(int age){
+            this.age = age;
+            return this;
+        }
+
+        public RenterBuilder setGender(String gender){
+            this.gender = gender;
+            return this;
+        }
+
+        public RenterBuilder setAdress(String adress){
+            this.adress = adress;
+            return this;
+        }   
+
+        public RenterBuilder setNumTel(String num){
+            this.numTel = num;
+            return this;
+        }
+
+        public RenterBuilder setOwnedBike(Bike bike){
+            this.ownedBike = bike;
+            return this;
+        }
+
+        public Renter build(){
+            return new Renter(this);
+        }
+        
+    }
+}
