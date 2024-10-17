66dd30be5553dadb3da1c81235c6a31b663243e0
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-10-17 13:12:17 +0200

    classe Flashlight

---
+++ src/VLille/vehicle/Bike/equipment/Flashlight.java
@@ -0,0 +1,21 @@
+package VLille.vehicle.Bike.equipment;
+import VLille.vehicle.*;
+import VLille.vehicle.Bike.*;
+
+public class Flashlight extends DecorateurBike {
+    public Flashlight (Bike b){
+        bike = b;
+    }
+
+    public String getLibelle() {
+        return bike.getLibelle() + "+ Flashlight ";
+    }
+
+    public int getNbUsage(){
+        return bike.getNbUsage();
+    }
+
+    public String getBikeId(){
+        return bike.getBikeId();
+    }
+}
