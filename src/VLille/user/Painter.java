65a3a93c45dc6a26ebd268b31eea76b7015b0b04
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-10-17 12:56:33 +0200

    classe Painter

---
+++ src/VLille/user/Painter.java
@@ -0,0 +1,65 @@
+package VLille.user;
+import VLille.vehicle.Bike.*;
+
+public class Painter extends User{
+
+    public Painter (PainterBuilder builder){
+        this.firstName = builder.firstName;
+        this.secondName = builder.secondName;
+        this.age = builder.age;
+        this.gender = builder.gender;
+        this.adress = builder.adress;
+        this.numTel = builder.numTel;
+        this.ownedBike = builder.ownedBike;
+    }
+
+    public static class PainterBuilder {
+        private String firstName;
+        private String secondName;
+        private int age;
+        private String gender;
+        private String adress;
+        private String numTel;
+        private Bike ownedBike; 
+
+        public PainterBuilder setFirstName(String name){
+            this.firstName = name;
+            return this;
+        }
+
+        public PainterBuilder setSecondName(String name){
+            this.secondName = name;
+            return this;
+        }
+
+        public PainterBuilder setAge(int age){
+            this.age = age;
+            return this;
+        }
+
+        public PainterBuilder setGender(String gender){
+            this.gender = gender;
+            return this;
+        }
+
+        public PainterBuilder setAdress(String adress){
+            this.adress = adress;
+            return this;
+        }   
+
+        public PainterBuilder setNumTel(String num){
+            this.numTel = num;
+            return this;
+        }
+
+        public PainterBuilder setOwnedBike(Bike bike){
+            this.ownedBike = bike;
+            return this;
+        }
+
+        public Painter build(){
+            return new Painter(this);
+        }
+        
+    }
+}
