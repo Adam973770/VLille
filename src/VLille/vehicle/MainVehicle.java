7d169fd3f77c9403b3b4c6388ff720f2958d4330
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-11-01 20:05:03 +0100

    exemple d'utilisation

--- src/VLille/vehicle/MainVehicle.java
+++ src/VLille/vehicle/MainVehicle.java
@@ -5,12 +5,12 @@ import VLille.vehicle.Bike.equipment.*;
 
 public class MainVehicle {
     public static void main (String[] args){
-        Bike b1 = new ClassicBike("bike1");
+        Bike b1 = new ClassicBike();
         b1 = new Basket(b1);
         b1 = new Bottle(b1);
         System.out.println(b1);
 
-        Bike b2 = new ElectricBike("bike2");
+        Bike b2 = new ElectricBike();
         b2 = new Basket(b2);
         b2 = new LuggageRack(b2);
         System.out.println(b2);
