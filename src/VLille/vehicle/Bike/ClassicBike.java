4706a3ca4741bc466eaf9bd3372e6998c3ef7c0e
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-11-01 20:07:53 +0100

    ClassicBike

--- src/VLille/vehicle/Bike/ClassicBike.java
+++ src/VLille/vehicle/Bike/ClassicBike.java
@@ -2,12 +2,9 @@ package VLille.vehicle.Bike;
 import VLille.vehicle.*;
 
 public class ClassicBike extends Bike {
-        
-    public static final int LIMIT_OF_USAGE = 5;
 
-    public ClassicBike(String classicBikeId) {
+    public ClassicBike() {
+        super();
         this.libelle = "Classic Bike ";
-        this.bikeId = classicBikeId;
-        this.nbUsage = 0;
     }
 }
