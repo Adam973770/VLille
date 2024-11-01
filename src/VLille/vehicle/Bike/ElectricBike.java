a781b8784dc772c4b4fb3f51ad16cad24bad0748
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-11-01 20:07:28 +0100

    ElectricBike

--- src/VLille/vehicle/Bike/ElectricBike.java
+++ src/VLille/vehicle/Bike/ElectricBike.java
@@ -2,12 +2,9 @@ package VLille.vehicle.Bike;
 import VLille.vehicle.*;
 
 public class ElectricBike extends Bike {
-        
-    public static final int LIMIT_OF_USAGE = 5;
 
-    public ElectricBike(String electricBikeId) {
+    public ElectricBike() {
+        super();
         this.libelle = "Electric Bike ";
-        this.bikeId = electricBikeId;
-        this.nbUsage = 0;
     }
 }
