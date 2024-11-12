27ea9b7be75ea18bc9d1ebb38b8bf3abb28c92ff
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-11-12 18:49:38 +0100

    implementation avec le pattern Observer

--- src/VLille/controlCenter/ControlCenter.java
+++ src/VLille/controlCenter/ControlCenter.java
@@ -12,10 +12,12 @@ public class ControlCenter{
 
     private ArrayList<Station> allStation;
     private Map<User, Vehicle> allNotAvailableVehicles;
+    private ArrayList<ControlCenterObserver> observers; 
 
     private ControlCenter(){
         this.allStation = new ArrayList<Station>();
         this.allNotAvailableVehicles = new HashMap<User, Vehicle>();
+        this.observers = new ArrayList<>();
     }
 
     public static ControlCenter getInstance(){
@@ -32,4 +34,34 @@ public class ControlCenter{
     public Map<User, Vehicle> getAllNotAvailableVehicles(){
         return this.allNotAvailableVehicles;
     }
+
+    public void addObserver(ControlCenterObserver observer){
+        this.observers.add(observer);
+    }
+
+    public void removeObserver(ControlCenterObserver observer){
+        this.observers.remove(observer);
+    }
+
+    public void notificationObserver(String message) {
+        for (ControlCenterObserver observer : observers) {
+            observer.update(message);
+        }
+    }
+
+    public void addStation(Station station){
+        this.allStation.add(station);
+        notificationObserver(station.toString() + " a été ajouté au centre de controle");
+    }
+
+    public void removeStation(Station station){
+        this.allStation.remove(station);
+        notificationObserver(station.toString() + " a été supprimé du centre de controle");
+    }
+
+    public void addNotAvailableVehicle(User user, Vehicle vehicle) {
+        this.allNotAvailableVehicles.put(user, vehicle);
+        notificationObserver(vehicle.toString() + " n'est pas disponible");
+    }
+
 }
