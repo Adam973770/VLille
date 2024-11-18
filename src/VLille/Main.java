d76e5916854f48e45b115f4217cf34f16fe3efa9
Ramjattan Adam <adam.ramjattan.etu@a10p11.fil.univ-lille.fr>
2024-11-18 12:23:43 +0100

    main

--- src/VLille/Main.java
+++ src/VLille/Main.java
@@ -4,6 +4,10 @@ import VLille.Station.*;
 import java.util.*;
 import VLille.vehicle.*;
 import VLille.user.*;
+import VLille.controlCenter.*;
+import VLille.vehicle.Bike.*;
+import VLille.vehicle.Bike.equipment.*;
+
 public class Main {
     public static void main(String [] args){
         //user
@@ -17,11 +21,11 @@ public class Main {
         Bike b1 = new ClassicBike();
         b1 = new Basket(b1);
         b1 = new Bottle(b1);
-        System.out.println(b1);
+        System.out.println(b1.toString());
 
         Bike b2 = new ElectricBike();
         b2 = new Basket(b2);
         b2 = new LuggageRack(b2);
-        System.out.println(b2);
+        System.out.println(b2.toString());
     }
 }
