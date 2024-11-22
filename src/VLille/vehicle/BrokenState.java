a5f6a2fa9d8f3c19622fd55251b4660adf85c410
Ramjattan Adam <adam.ramjattan.etu@a14p7.fil.univ-lille.fr>
2024-11-22 09:32:21 +0100

    BrokenState class

--- src/VLille/vehicle/BrokenState.java
+++ src/VLille/vehicle/BrokenState.java
@@ -1,8 +1,44 @@
 package VLille.vehicle;
 
 /**
- * State of a vehicle which change depending on the context
+ * State of a vehicle which represent the context broken
  */
 public class BrokenState extends State{
-    
+    /**
+     * Construct a new `BrokenState` Object
+     * 
+     * @param vehicle the object that gonna have this state
+     */
+    public BrokenState(Vehicle vehicle){
+        super(vehicle);
+        this.stateDescription = "Broken";
+    }
+
+    /**
+     * create a new state for the vehicle representing a rented state
+     */
+    public void rented(){
+        this.vehicle.setState(new RentedState(this.vehicle));
+    }
+
+    /**
+     * create a new state for the vehicle representing a not rented state
+     */
+    public void notRented(){
+        this.vehicle.setState(new NotRentedState(this.vehicle));
+    }
+
+    /**
+     * create a new state for the vehicle representing a stealed state
+     */
+    public void stealed(){
+        this.vehicle.setState(new StealedState(this.vehicle));
+    }
+
+    /**
+     * explain that the vehicle is already broken
+     */
+    public void broken(){
+        System.out.println("Already broken");
+    }
 }
