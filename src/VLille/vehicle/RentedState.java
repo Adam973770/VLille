da5d1a5c349e948cd04c5b88fc221d9366ab8501
Ramjattan Adam <adam.ramjattan.etu@a14p7.fil.univ-lille.fr>
2024-11-22 09:33:11 +0100

    RentedState class

---
+++ src/VLille/vehicle/RentedState.java
@@ -0,0 +1,44 @@
+package VLille.vehicle;
+
+/**
+ * State of a vehicle which represent the context not rented
+ */
+public class RentedState extends State{
+    /**
+     * Construct a new `RentedState` Object
+     * 
+     * @param vehicle the object that gonna have this state
+     */
+    public RentedState(Vehicle vehicle){
+        super(vehicle);
+        this.stateDescription = "Rented";
+    }
+
+    /**
+     * explain that the vehicle is already rented
+     */
+    public void rented(){
+        System.out.println("Already rented");
+    }
+
+    /**
+     * create a new state for the vehicle representing a not rented state
+     */
+    public void notRented(){
+        this.vehicle.setState(new NotRentedState(this.vehicle));
+    }
+
+    /**
+     * create a new state for the vehicle representing a stealed state
+     */
+    public void stealed(){
+        this.vehicle.setState(new StealedState(this.vehicle));
+    }
+
+    /**
+     * create a new state for the vehicle representing a broken state
+     */
+    public void broken(){
+        this.vehicle.setState(new BrokenState(this.vehicle));
+    }
+}
