3e09ca330d2fde7bfdad846f5b056ea88ad4f375
Ramjattan Adam <adam.ramjattan.etu@a14p7.fil.univ-lille.fr>
2024-11-22 09:34:04 +0100

    StealedState class

---
+++ src/VLille/vehicle/StealedState.java
@@ -0,0 +1,44 @@
+package VLille.vehicle;
+
+/**
+ * State of a vehicle which represent the context not stealed
+ */
+public class StealedState extends State{
+    /**
+     * Construct a new `StealedState` Object
+     * 
+     * @param vehicle the object that gonna have this state
+     */
+    public StealedState(Vehicle vehicle){
+        super(vehicle);
+        this.stateDescription = "Stealed";
+    }
+
+    /**
+     * create a new state for the vehicle representing a rented state
+     */
+    public void rented(){
+        this.vehicle.setState(new RentedState(this.vehicle));
+    }
+
+    /**
+     * create a new state for the vehicle representing a not rented state
+     */
+    public void notRented(){
+        this.vehicle.setState(new NotRentedState(this.vehicle));
+    }
+
+    /**
+     * explain that the vehicle is already stealed
+     */
+    public void stealed(){
+        System.out.println("Already stealed");
+    }
+
+    /**
+     * create a new state for the vehicle representing a broken state
+     */
+    public void broken(){
+        this.vehicle.setState(new BrokenState(this.vehicle));
+    }
+}
