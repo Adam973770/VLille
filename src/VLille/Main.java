61787bbeda4bad872492e3f9b49c0d6790305328
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-11-30 16:51:56 +0100

    quelque tests

--- src/VLille/Main.java
+++ src/VLille/Main.java
@@ -7,25 +7,39 @@ import VLille.user.*;
 import VLille.controlCenter.*;
 import VLille.vehicle.Bike.*;
 import VLille.vehicle.Bike.equipment.*;
+import VLille.Exceptions.*;
 
 public class Main {
-    public static void main(String [] args){
+    public static void main(String [] args) throws StationIsAlreadyEmpty, StationIsFullException{
         //user
         Painter painter = new Painter.PainterBuilder().setFirstName("Jeanne").setGender("Femme").build();
-        System.out.println(painter.getFirstName());
         Renter renter = new Renter.RenterBuilder().setFirstName("Theo").setGender("Homme").build();
-        System.out.println(renter.getFirstName());
         Repairer repairer = new Repairer.RepairerBuilder().setFirstName("Theophane").setGender("Homme").build();
-        System.out.println(repairer.getFirstName());
+        Thief thief = new Thief.ThiefBuilder().setFirstName("Leo").setGender("Homme").build();
+
         //vehicle
         Bike b1 = new ClassicBike();
         b1 = new Basket(b1);
         b1 = new Bottle(b1);
-        System.out.println(b1.toString());
 
         Bike b2 = new ElectricBike();
         b2 = new Basket(b2);
         b2 = new LuggageRack(b2);
-        System.out.println(b2.toString());
+
+        //station
+        ControlCenter controlCenter = ControlCenter.getInstance();
+        Station bikeStation = new Station(controlCenter);
+        bikeStation.dropVehicle(b1);
+
+        //action User
+        ActionUserlmpl actionUser = new ActionUserlmpl();
+        actionUser.rent(repairer, bikeStation);
+        System.out.println(repairer.getOwnedVehicle());
+        System.out.println(b1.getCurrentRenter());
+        actionUser.drop(repairer, bikeStation);
+        System.out.println(repairer.getOwnedVehicle());
+        System.out.println(b1.getCurrentRenter());
+        System.out.println(b1.toString());
+
     }
 }
