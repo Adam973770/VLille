7bf3b3284f9b92e94d9c589b75b4fc7038a7645c
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-11-30 17:00:51 +0100

    javadoc

--- src/VLille/user/User.java
+++ src/VLille/user/User.java
@@ -1,40 +1,86 @@
 package VLille.user;
 import VLille.vehicle.Bike.*;
+import VLille.vehicle.*;
 
+/**
+ * Abstract class representing a user in the VLille system
+ */
 public abstract class User{
+    /** The first name of the user. */
     protected String firstName;
+    /** The second name (last name) of the user. */
     protected String secondName;
+    /** The age of the user. */
     protected int age;
+    /** The gender of the user. */
     protected String gender;
+    /** The address of the user. */
     protected String address;
+    /** The phone number of the user. */
     protected String numTel;
-    protected Bike ownedBike;
+    /** The vehicle currently owned or rented by the user. */
+    protected Vehicle ownedVehicle;
 
-    public String getFirstName(){
+    /**
+     * Gets the first name of the user.
+     * 
+     * @return The first name of the user.
+     */
+    public String getFirstName() {
         return firstName;
     }
 
-    public String getSecondName(){
+    /**
+     * Gets the second name of the user.
+     * 
+     * @return The second name of the user.
+     */
+    public String getSecondName() {
         return secondName;
     }
 
-    public int getAge(){
+    /**
+     * Gets the age of the user.
+     * 
+     * @return The age of the user.
+     */
+    public int getAge() {
         return age;
     }
 
-    public String getGender(){
+    /**
+     * Gets the gender of the user.
+     * 
+     * @return The gender of the user.
+     */
+    public String getGender() {
         return gender;
     }
 
-    public String getAddress(){
+    /**
+     * Gets the address of the user.
+     * 
+     * @return The address of the user.
+     */
+    public String getAddress() {
         return address;
     }
 
-    public String getNumTel(){
+    /**
+     * Gets the phone number of the user.
+     * 
+     * @return The phone number of the user.
+     */
+    public String getNumTel() {
         return numTel;
     }
 
-    public Bike getOwnedBike(){
-        return ownedBike;
+    /**
+     * Gets the vehicle currently owned or rented by the user.
+     * 
+     * @return The vehicle owned or rented by the user, or null if no vehicle is owned.
+     */
+    public Vehicle getOwnedVehicle() {
+        return ownedVehicle;
     }
 }
