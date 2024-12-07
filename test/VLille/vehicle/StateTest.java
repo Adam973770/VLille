abc5b2108f30b1a7c9e73ca3c42ba847c1fdf8c1
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-12-07 21:53:07 +0100

    test de toute les methodes de changement de State

--- test/VLille/vehicle/StateTest.java
+++ test/VLille/vehicle/StateTest.java
@@ -4,14 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 import src.VLille.vehicle.*;
-
+import src.VLille.vehicle.Bike.*;
 
 public abstract class StateTest {
     protected State state;
+    protected Vehicle vehicle;
 
     @BeforeEach
     public void init(){
         this.state = createState();
+        this.vehicle = new ClassicBike();
+        this.state.setVehicle(this.vehicle);
     }
 
     protected abstract State createState();
@@ -20,5 +23,23 @@ public abstract class StateTest {
     public void rentedStateChangeCheck(){
         this.state.rented();
         assertEquals(this.state.getStateDescription(), "Rented");
+    }
+
+    @Test
+    public void notRentedStateChangeCheck(){
+        this.state.notRented();
+        assertEquals(this.state.getStateDescription(), "Not rented");
+    }
+
+    @Test
+    public void brokenStateChangeCheck(){
+        this.state.broken();
+        assertEquals(this.state.getStateDescription(), "Broken");
+    }
+
+    @Test
+    public void stealedStateChangeCheck(){
+        this.state.stealed();
+        assertEquals(this.state.getStateDescription(), "Stealed");
     }
 }
