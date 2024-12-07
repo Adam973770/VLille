962bbbd86fa36e0b94e06d93d28aba013f8f2f68
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2024-12-07 22:35:06 +0100

    Test par heritage de la classe VehicleTest

---
+++ test/VLille/vehicle/Bike/BikeTest.java
@@ -0,0 +1,10 @@
+package test.VLille.vehicle;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import src.VLille.vehicle.*;
+
+public abstract class BikeTest extends VehicleTest{
+
+}
